7debcd9dd55bae7bea80014d56f1a3a648ffce3f
nirqqen97 <93587073+nirqqen97@users.noreply.github.com>
2023-05-24 14:43:57 +0300

    Add Header

---
+++ index.js
@@ -0,0 +1,26 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = "6146057071:AAFnH4AC-dTWv4LODq9fgN8kP6_5vl7Iolg"
+
+const bot = new TelegramBot(token, { polling: true });
+
+const webAppUrl = 'https://646df0d49ad853000887936a--incomparable-selkie-7724d9.netlify.app/';
+
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    await bot.sendMessage(chatId, text);
+    if (text === '/start') {
+        await bot.sendMessage(chatId, "появится кнопка снизу", {
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: 'do a order', web_app: { url: webAppUrl } }]
+                ]
+            }
+        });
+    }
+  
+});
+
+
