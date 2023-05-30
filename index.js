b9e2a14c5b655e5a3c3b212e48195e82bb323677
nirqqen97 <93587073+nirqqen97@users.noreply.github.com>
2023-05-30 22:33:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,21 +6,31 @@ const bot = new TelegramBot(token, { polling: true });
 
 const webAppUrl = 'https://incomparable-selkie-7724d9.netlify.app/';
 
-bot.on('message', async (msg) => {
-    const chatId = msg.chat.id;
-    const text = msg.text;
 
-    await bot.sendMessage(chatId, text);
+bot.on('message', async (msg) => { 
+    const chatId = msg.chat.id
+    const text = msg.text
+
     if (text === '/start') {
-        await bot.sendMessage(chatId, "появится кнопка снизу", {
-            reply_markup: {
-                inline_keyboard: [
-                    [{ text: 'do a order', web_app: { url: webAppUrl } + '/form' }]
+        await bot.sendMessage(chatId, "button will be bottom", {
+            reply_markup : {
+                inline_keyboard : [
+                    [{text : 'do a order', web_app: {url : webAppUrl }}]
                 ]
             }
-        });
+        })
+        await bot.sendMessage(chatId, 'keyboard Button', {
+            reply_markup : {
+                keyboard : [
+                    [{text : 'do a order', web_app : {url : webAppUrl }}]
+                ]
+            }
+        })
     }
-  
-});
+    if (msg?.web_app_data?.data) {                                  //* обрабатывать данные которые пришли с фронта
+        const data = JSON.parse(msg?.web_app_data?.data)
+        await bot.sendMessage(chatId, `your city ${data.city}`)
+    }
+})
 
 
